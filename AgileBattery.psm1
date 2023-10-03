de596f077f969f2fc24dd638c25af0019b4a97fc
halfrubbish <github@mattpacker.com>
2023-10-03 21:38:21 +0100

    Update AgileBattery.psm1
    hopefully working version

--- AgileBattery.psm1
+++ AgileBattery.psm1
@@ -1,7 +1,7 @@
 $teslaBaseURI = "https://owner-api.teslamotors.com/"
 $teslaAuthURI = "https://auth.tesla.com/oauth2/v3/token"
      
-function Get-TeslaApiToken{
+function Get-TeslaApiToken {
     <#
         .SYNOPSIS 
         Retreives a Tesla API token when provided with a valid refresh token
@@ -18,23 +18,24 @@ function Get-TeslaApiToken{
     try {
   
         $params = @{
-            "grant_type" ="refresh_token"
-            "client_id"= "ownerapi"
-            "refresh_token"= "$refreshToken"
-            "scope"= "openid email offline_access"
+            "grant_type"    = "refresh_token"
+            "client_id"     = "ownerapi"
+            "refresh_token" = "$refreshToken"
+            "scope"         = "openid email offline_access"
         }
   
         $resp = Invoke-RestMethod -Method POST -Uri $teslaAuthURI -Body $params
 
         $resp.access_token
 
-    }catch{
+    }
+    catch {
         throw "Could not retrieve a valid refresh token: $_"
     }
 
 }
 
-function Get-OctopusAgilePricing{
+function Get-OctopusAgilePricing {
     <#
     .SYNOPSIS 
     Retrieves the Octopus Agile pricing tariff data
@@ -62,8 +63,8 @@ function Get-OctopusAgilePricing{
     #>
     [CmdletBinding()]
     param(
-        [Parameter(Position=0)]
-        [ValidateSet("A","B","C","D","E","F","G","H","J","K","L","M","N","P")]
+        [Parameter(Position = 0)]
+        [ValidateSet("A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P")]
         [char]$DistributionNetworkOperator
     )
 
@@ -80,21 +81,21 @@ function Get-OctopusAgilePricing{
   
 }
 
-function Get-TeslaProducts{
+function Get-TeslaProducts {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [string]$ApiToken
     )
 
-   (Invoke-RestMethod -Method Get -Headers @{ "Authorization"="Bearer $ApiToken" } -Uri "$teslaBaseURI/api/1/products").response
+   (Invoke-RestMethod -Method Get -Headers @{ "Authorization" = "Bearer $ApiToken" } -Uri "$teslaBaseURI/api/1/products").response
   
 }
 
-function Get-TeslaEnergySiteId{
+function Get-TeslaEnergySiteId {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [string]$ApiToken
     )
 
@@ -105,92 +106,117 @@ function Get-TeslaEnergySiteId{
 function Get-TeslaPowerWallId {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [string]$ApiToken
     )
 
     $response = Get-TeslaProducts $ApiToken
-    $battery = $response | Where-Object {$_.resource_type -eq "battery"} | Select-Object -first 1
+    $battery = $response | Where-Object { $_.resource_type -eq "battery" } | Select-Object -first 1
     $battery.id 
 }
 
-function Get-TeslaPowerWallStatus{
+function Get-TeslaPowerWallStatus {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [string]$ApiToken
     )
-    Invoke-RestMethod -method Get -URI "$teslaBaseURI/api/1/energy_sites/$energy_site_id/site_info"  -Headers @{ "Authorization"="Bearer $ApiToken" }
+    Invoke-RestMethod -method Get -URI "$teslaBaseURI/api/1/energy_sites/$energy_site_id/site_info"  -Headers @{ "Authorization" = "Bearer $ApiToken" }
 
 }
 
 # function to control the powerwall 
-function Set-TeslaEnergySiteReservePower{
+function Set-TeslaEnergySiteReservePower {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [string]$ApiToken,
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [Parameter(Mandatory, ValueFromPipeline, Position = 1)]
         [string]$EnergySiteId,
-        [Parameter(Mandatory,Position=1)]
+        [Parameter(Mandatory, Position = 2)]
         [int]$ReservePowerPercentage
     )
-
-    Invoke-RestMethod -Method Post -Headers @{ "Authorization"="Bearer $ApiToken" } -body ( @{"backup_reserve_percent"=$ReservePowerPercentage}|convertto-json) -Uri "$teslaBaseURI/api/1/energy_sites/$EnergySiteId/backup" -ContentType 'application/json'
+    
+    Invoke-RestMethod -Method Post -Headers @{ "Authorization" = "Bearer $ApiToken" } -body ( @{"backup_reserve_percent" = $ReservePowerPercentage } | convertto-json) -Uri "$teslaBaseURI/api/1/energy_sites/$EnergySiteId/backup" -ContentType 'application/json' | out-null
 
 }
 
 
 #main loop
-function Invoke-AgileBatteryControl{
+function Invoke-AgileBatteryControl {
     [CmdletBinding()]
     param(
         [Parameter()]
         [string]$RefreshToken,
 
         [Parameter()]
-        [char]$DNORegion = "J"
+        [string]$RefreshTokenPath = "token.txt",
+
+        [Parameter()]
+        [char]$DNORegion = "J",
+
+        [Parameter()]
+        [int]$HoursToChargePowerWall = 5,
+
+        [Parameter()]
+        [Boolean]$Loop = $true
     )
 
-    if(!$RefreshToken){
-        #try loading from token.txt
+    if (-not $RefreshToken) {
+        #try loading from token.txt if none specified on command line
+        try {
+            $RefreshToken = Get-Content $RefreshTokenPath -ErrorAction Stop
+        }
+        catch {
+            throw "Could not load token from $RefreshTokenPath and no `$refreshToken was specified on the command line, aborting"
+        }
     }
 
-    #try getting an API token now, else bomb out
+    # get Site Id                
+    $energySiteId = [string](Get-TeslaEnergySiteId -ApiToken $apiToken)
+            
+    if (!$energySiteId) {
+        $loop = $false
+        throw "Could not retrieve energy site information, aborting"
+    }
 
-    #try getting other static essentials, else bomb out
-
-    $prices = "ff"
-
-
-    # have everything we need now, so we can do the main loop
-    $loop = $true
-    $powerwallId = $null
     
-    while($loop){
+    do {
         try {
-            # get prices 
-            $currentPrices = Get-OctopusAgilePricing $DNORegion
 
             # refresh API token
             $apiToken = Get-TeslaApiToken $RefreshToken
 
-                
-                $powerwallId = Get-TeslaPowerWallId -ApiToken $apiToken
+            # get the time 
+            $now = [DateTime]::UtcNow 
 
-                if(!$powerwallId){
-                    $loop = $false
-                    throw "Could not retrieve powerwall information, aborting"
-                }
-
+            # get prices 
+            $currentPrices = Get-OctopusAgilePricing $DNORegion
+            $todaysPrices = $currentPrices | Where-Object { ([DateTime]$_.valid_from).ToUniversalTime().Date -eq $now.Date.ToUniversalTime() } 
+            $cheapestHalfHourZones = $todaysPrices  | Sort-Object value_inc_vat | Select-Object -first ($HoursToChargePowerWall * 2)
+            $cheapestPriceStartingZone = $cheapestHalfHourZones | Select-Object -ExpandProperty value_inc_vat -last 1 
             
+            # work out if current time slot is in the cheap zone of the cheapest hours we want to charge the powerwall
+            $currentTimeSlot = $todaysPrices | Where-Object { ($now -gt ([datetime]$_.valid_from).ToUniversalTime()) -and ($now -lt ([datetime]$_.valid_to).ToUniversalTime()) }
+            $isCheapTimeSlot = $cheapestHalfHourZones | Where-Object { ($now -gt ([datetime]$_.valid_from).ToUniversalTime()) -and ($now -lt ([datetime]$_.valid_to).ToUniversalTime()) }
+            $price = $currentTimeSlot.value_inc_vat
+            
+            if ($isCheapTimeSlot) {
+                Write-Host "Cheapest time found :: $now :: $price :: Todays cheapest prices begin at $cheapestPriceStartingZone"
 
+                # set powerwall to 100% reserve
+                Set-TeslaEnergySiteReservePower -ApiToken $apiToken -EnergySiteId $energySiteId -ReservePowerPercentage 100
+            }
+            else {
+                Write-host "Time Found but not in cheapest zone :: $now :: $price :: Todays cheapest prices begin at $cheapestPriceStartingZone"
+
+                # set powerwall to 20% reserve
+                Set-TeslaEnergySiteReservePower -ApiToken $apiToken -EnergySiteId $energySiteId -ReservePowerPercentage 20
+            }
         }
         catch {
-            Clear-Host
-            Write-Host "Encountered an error on this run, but will retry in 30s: $_"
-            
+            Write-Host "Encountered an error on this run, but will retry in 30s: $_"            
         }
         Start-Sleep -Seconds 30
-    }
+    }while ($loop)
 }
